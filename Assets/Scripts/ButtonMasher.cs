62b723a6e104c5fbad86ed30f4909ebee5214d4d
Nyyani <43067342+Nyyani@users.noreply.github.com>
2018-09-14 15:10:29 +0100

    Fixed Timer Slightly

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -6,17 +6,28 @@ public class ButtonMasher : MonoBehaviour {
 
     public AudioSource clickSound; //sound that will play when we click the button
     public TextMesh scoreText; // display text for the player's score
+    public TextMesh timerText; // display text for the player's timer remaining
+    public float gameLength; //How many seconds will the game last
 
     private int score = 0; // the numerical data value of our score
+    private float timeRemaining = 0; //Numerical time remaining
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Start method called");	
-	}//
+        Debug.Log("Start method called");
+
+        timeRemaining = gameLength;
+
+	}// End of start
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log("Start method updated");
+
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        timerText.text = (Mathf.CeilToInt(timeRemaining)).ToString(); 
+
 	}//End of Update
 
     void OnMouseDown()
